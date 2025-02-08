d0b1591cf532be2ad12eb33e8e9141fb879a5cec
SXX <sxx1136965276@gmail.com>
2025-02-08 14:36:46 +0800

    add aes dynamic

---
+++ src/crypto/aes/dynamic.rs
@@ -0,0 +1,69 @@
+use crate::is_hw_feature_available;
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+use super::x86 as hw;
+#[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
+use super::arm as hw;
+
+#[derive(Clone)]
+pub enum AES128 {
+    HW(hw::AES128),
+    SW(super::soft::AES128),
+}
+
+impl AES128 {
+    pub const BLOCK_LEN: usize = 16;
+    pub const KEY_LEN: usize = 16;
+    #[inline(always)]
+    pub fn new(key: [u8; 16]) -> Self {
+        if is_hw_feature_available!(
+            "x86" => ("aes", "sse2"),
+            "x86_64" => ("aes", "sse2"),
+            "aarch64" => ("aes")
+        ) {
+            AES128::HW(hw::AES128::new(key))
+        } else {
+            AES128::SW(super::soft::AES128::new(key))
+        }
+    }
+    #[inline(always)]
+    pub fn from_slice(key: &[u8]) -> Self {
+        if is_hw_feature_available!(
+            "x86" => ("aes", "sse2"),
+            "x86_64" => ("aes", "sse2"),
+            "aarch64" => ("aes")
+        ) {
+            AES128::HW(hw::AES128::from_slice(key))
+        } else {
+            AES128::SW(super::soft::AES128::from_slice(key))
+        }
+    }
+    #[inline(always)]
+    pub fn encrypt(&self, block: &mut [u8; 16]) {
+        match self {
+            AES128::HW(hw) => hw.encrypt(block),
+            AES128::SW(sw) => sw.encrypt(block),
+        }
+    }
+    #[inline(always)]
+    pub fn decrypt(&self, block: &mut [u8; 16]) {
+        match self {
+            AES128::HW(hw) => hw.decrypt(block),
+            AES128::SW(sw) => sw.decrypt(block),
+        }
+    }
+    #[inline(always)]
+    pub fn encrypt_copy(&self, block: &[u8; 16], output: &mut [u8; 16]) {
+        match self {
+            AES128::HW(hw) => hw.encrypt_copy(block, output),
+            AES128::SW(sw) => sw.encrypt_copy(block, output),
+        }
+    }
+    #[inline(always)]
+    pub fn decrypt_copy(&self, block: &[u8; 16], output: &mut [u8; 16]) {
+        match self {
+            AES128::HW(hw) => hw.decrypt_copy(block, output),
+            AES128::SW(sw) => sw.decrypt_copy(block, output),
+        }
+    }
+}
