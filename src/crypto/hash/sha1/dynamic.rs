c22160757d98a4bfa54d01844fb4c38bc16a5cea
SXX <sxx1136965276@gmail.com>
2025-02-26 10:59:05 +0800

    dynamic sha1 implementation

---
+++ src/crypto/hash/sha1/dynamic.rs
@@ -0,0 +1,100 @@
+use super::soft;
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+use super::x86;
+#[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
+use super::arm;
+
+#[derive(Clone, Copy)]
+pub union Sha1 {
+    soft: soft::Sha1,
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    x86_sha: x86::Sha1,
+    #[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
+    arm_sha: arm::Sha1,
+}
+
+static mut IDX: u32 = u32::MAX; // 0: soft, 1: x86/arm
+
+impl Sha1 {
+    pub const BLOCK_LEN: usize = 64;
+    pub const DIGEST_LEN: usize = 20;
+
+    const BLOCK_LEN_BITS: u64 = Self::BLOCK_LEN as u64 * 8;
+    const MLEN_SIZE: usize = core::mem::size_of::<u64>();
+    const MLEN_SIZE_BITS: u64 = Self::MLEN_SIZE as u64 * 8;
+    const MAX_PAD_LEN: usize = Self::BLOCK_LEN + Self::MLEN_SIZE as usize;
+
+    #[inline(always)]
+    pub fn new() -> Self {
+        unsafe {
+            if IDX == u32::MAX {
+                if crate::is_hw_feature_detected!(
+                    "x86" => ("sse2", "sha"),
+                    "x86_64" => ("sse2", "sha"),
+                    "aarch64" => ("sha2"),
+                    "arm" => ("neon", "sha2")
+                ) {
+                    IDX = 1;
+                } else {
+                    IDX = 0;
+                }
+            }
+
+            match IDX {
+                0 => Sha1 {
+                    soft: soft::Sha1::new(),
+                },
+                #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+                1 => Sha1 {
+                    x86_sha: x86::Sha1::new(),
+                },
+                #[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
+                1 => Sha1 {
+                    arm_sha: arm::Sha1::new(),
+                },
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    #[inline]
+    pub fn update(&mut self, data: &[u8]) {
+        unsafe {
+            match IDX {
+                0 => self.soft.update(data),
+                #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+                1 => self.x86_sha.update(data),
+                #[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
+                1 => self.arm_sha.update(data),
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    #[inline]
+    pub fn finalize(self) -> [u8; Self::DIGEST_LEN] {
+        unsafe {
+            match IDX {
+                0 => self.soft.finalize(),
+                #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+                1 => self.x86_sha.finalize(),
+                #[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
+                1 => self.arm_sha.finalize(),
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    pub fn oneshot<T: AsRef<[u8]>>(data: T) -> [u8; 20] {
+        unsafe {
+            match IDX {
+                0 => soft::Sha1::oneshot(data),
+                #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+                1 => x86::Sha1::oneshot(data),
+                #[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
+                1 => arm::Sha1::oneshot(data),
+                _ => unreachable!(),
+            }
+        }
+    }
+}
