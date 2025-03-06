a520616cb890ac1d65c5df2176f7293c1aa3ac80
SXX <sxx1136965276@gmail.com>
2025-03-07 02:12:50 +0800

    add macro loop_unroll

---
+++ macros/src/lib.rs
@@ -0,0 +1,91 @@
+use proc_macro::Group;
+use proc_macro::Ident;
+use proc_macro::Literal;
+use proc_macro::Punct;
+use proc_macro::Spacing;
+use proc_macro::Span;
+use proc_macro::TokenStream;
+use proc_macro::Delimiter;
+use proc_macro::TokenTree;
+
+#[proc_macro_attribute]
+pub fn loop_unroll(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let attr_iter: Vec<_> = attr.into_iter().collect();
+    let idx_var_name = if let TokenTree::Ident(ident) = &attr_iter[0] {
+        ident.clone()
+    } else {
+        panic!("First argument must be an identifier");
+    };
+    let idx_start = if let TokenTree::Literal(ident) = &attr_iter[2] {
+        ident.clone()
+    } else {
+        panic!("Second argument must be an Literal");
+    };
+    let loop_count = if let TokenTree::Literal(ident) = attr_iter[4].clone() {
+        ident
+    } else {
+        panic!("Third argument must be an identifier");
+    };
+
+    // test if idx_var_name is "_"
+    let no_need_idx_var = idx_var_name.to_string() == "_";
+
+    let mut output = TokenStream::new();
+
+    if !no_need_idx_var {
+        // output token "let" "mut" "idx_var_name" "=" "idx_start"";"
+        output.extend(TokenStream::from(TokenTree::Ident(Ident::new("let", Span::call_site()))));
+        output.extend(TokenStream::from(TokenTree::Ident(Ident::new("mut", Span::call_site()))));
+        output.extend(TokenStream::from(TokenTree::Ident(idx_var_name.clone())));
+        output.extend(TokenStream::from(TokenTree::Punct(Punct::new('=', Spacing::Alone))));
+        output.extend(TokenStream::from(TokenTree::Literal(idx_start)));
+        output.extend(TokenStream::from(TokenTree::Punct(Punct::new(';', Spacing::Alone))));
+    }
+
+    // parse loop_count as usize
+    let loop_count = loop_count.to_string().parse::<usize>().unwrap();
+    
+    let mut prefix_count = 0;
+    // ignore Ident before brace
+    for tt in item.clone().into_iter() {
+        if let TokenTree::Group(ref group) = tt {
+            if group.delimiter() == Delimiter::Brace {
+                break;
+            }
+        }
+        prefix_count += 1;
+    }
+
+    let mut iter = item.into_iter();
+    for _ in 0..prefix_count {
+        iter.next();
+    }
+
+    for _ in 0..(loop_count - 1) {
+        let item = iter.clone();
+        // output token "{"
+        // output.extend(TokenStream::from(TokenTree::Group(Group::new(Delimiter::Brace, TokenStream::new()))));
+        for tt in item {
+            output.extend(TokenStream::from(tt));
+        }
+        if !no_need_idx_var {
+            // output token "idx_var_name" "+=" "1"";"
+            output.extend(TokenStream::from(TokenTree::Ident(idx_var_name.clone())));
+            output.extend(TokenStream::from(TokenTree::Punct(Punct::new('+', Spacing::Joint))));
+            output.extend(TokenStream::from(TokenTree::Punct(Punct::new('=', Spacing::Alone))));
+            output.extend(TokenStream::from(TokenTree::Literal(Literal::usize_unsuffixed(1))));
+            output.extend(TokenStream::from(TokenTree::Punct(Punct::new(';', Spacing::Alone))));
+        }
+    }
+
+    for tt in iter {
+        output.extend(TokenStream::from(tt));
+    }
+
+    // output token }
+    output =TokenStream::from(TokenTree::Group(Group::new(Delimiter::Brace, output)));
+
+    // println!("output: {:?}", output.to_string());
+
+    output
+}
