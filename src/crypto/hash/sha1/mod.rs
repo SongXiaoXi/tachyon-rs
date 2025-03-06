d74aebcf10b5c8c0b12716d39d33f9192005d5fd
SXX <sxx1136965276@gmail.com>
2025-03-06 12:06:52 +0800

    add sha1 mod.rs

---
+++ src/crypto/hash/sha1/mod.rs
@@ -0,0 +1,20 @@
+pub mod soft;
+pub mod dynamic;
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+pub mod x86;
+#[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
+pub mod arm;
+
+cfg_if::cfg_if! {
+    if #[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "sse2", target_feature = "sha"))] {
+        pub use x86::*;
+    } else if #[cfg(all(any(target_arch = "aarch64", target_arch = "arm"), target_feature = "neon", target_feature = "sha2"))] {
+        pub use arm::*;
+    } else {
+        #[cfg(not(feature = "disable_dynamic_export"))]
+        pub use dynamic::*;
+        #[allow(unused_imports)]
+        #[cfg(feature = "disable_dynamic_export")]
+        pub(crate) use dynamic::*;
+    }
+}
