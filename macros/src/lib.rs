23b35f55421c23d681e757633e90fe1a3cc67acc
SXX <sxx1136965276@gmail.com>
2025-03-09 10:48:09 +0800

    add const_loop macro

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -86,9 +86,132 @@ pub fn loop_unroll(attr: TokenStream, item: TokenStream) -> TokenStream {
         output.extend(TokenStream::from(tt));
     }
 
-    output =TokenStream::from(TokenTree::Group(Group::new(Delimiter::Brace, output)));
+    output = TokenStream::from(TokenTree::Group(Group::new(Delimiter::Brace, output)));
 
     // println!("output: {:?}", output.to_string());
+
+    output
+}
+
+#[proc_macro]
+pub fn const_loop(item: TokenStream) -> TokenStream {
+    let mut iter = item.clone().into_iter();
+    let idx_var_name = if let TokenTree::Ident(ident) = iter.next().unwrap() {
+        ident.clone()
+    } else {
+        panic!("First argument must be an identifier");
+    };
+
+    if let TokenTree::Punct(punct) = iter.next().unwrap() {
+        if punct.as_char() != ',' {
+            panic!("Missing comma after argument");
+        }
+    } else {
+        panic!("Missing comma after argument");
+    }
+
+    let idx_start = if let TokenTree::Literal(ident) = iter.next().unwrap() {
+        ident.clone()
+    } else {
+        panic!("Second argument must be an integer literal");
+    };
+
+    if let TokenTree::Punct(punct) = iter.next().unwrap() {
+        if punct.as_char() != ',' {
+            panic!("Missing comma after argument");
+        }
+    } else {
+        panic!("Missing comma after argument");
+    }
+
+    let loop_count = if let TokenTree::Literal(ident) = iter.next().unwrap() {
+        ident
+    } else {
+        panic!("Third argument must be an integer identifier");
+    };
+
+    if let TokenTree::Punct(punct) = iter.next().unwrap() {
+        if punct.as_char() != ',' {
+            panic!("Missing comma after argument");
+        }
+    } else {
+        panic!("Missing comma after argument");
+    }
+
+    let next = iter.next().unwrap();
+
+    let mut block = None;
+
+    let loop_step = if let TokenTree::Literal(ident) = next {
+        ident.clone()
+    } else if let TokenTree::Group(group) = next {
+        if group.delimiter() == Delimiter::Brace {
+            block = Some(group);
+            Literal::usize_unsuffixed(1)
+        } else {
+            panic!("Fourth argument must be an integer identifier or a block");
+        }
+    } else {
+        panic!("Fourth argument must be an integer identifier or a block");
+    };
+
+    if block.is_none() {
+        if let TokenTree::Punct(punct) = iter.next().unwrap() {
+            if punct.as_char() != ',' {
+                panic!("Missing comma after argument 4");
+            }
+        } else {
+            panic!("Missing comma after argument 5");
+        }
+
+        let next = iter.next().unwrap();
+
+        if let TokenTree::Group(group) = next {
+            if group.delimiter() == Delimiter::Brace {
+                block = Some(group);
+            } else {
+                panic!("Fifth argument must be a block");
+            }
+        } else {
+            panic!("Fifth argument must be a block");
+        }
+    }
+
+    let block = block.unwrap();
+
+    // test if idx_var_name is "_"
+    let no_need_idx_var = idx_var_name.to_string() == "_";
+
+    let mut output = TokenStream::new();
+
+    if !no_need_idx_var {
+        // output token "let" "mut" "idx_var_name" "=" "idx_start"";"
+        output.extend(TokenStream::from(TokenTree::Ident(Ident::new("let", Span::call_site()))));
+        output.extend(TokenStream::from(TokenTree::Ident(Ident::new("mut", Span::call_site()))));
+        output.extend(TokenStream::from(TokenTree::Ident(idx_var_name.clone())));
+        output.extend(TokenStream::from(TokenTree::Punct(Punct::new('=', Spacing::Alone))));
+        output.extend(TokenStream::from(TokenTree::Literal(idx_start)));
+        output.extend(TokenStream::from(TokenTree::Punct(Punct::new(';', Spacing::Alone))));
+    }
+
+    // parse loop_count as usize
+    let loop_count = loop_count.to_string().parse::<usize>().unwrap();
+
+    for _ in 0..(loop_count - 1) {
+        output.extend(TokenStream::from(TokenTree::Group(block.clone())));
+        if !no_need_idx_var {
+            // output token "idx_var_name" "+=" "loop_step"";"
+            output.extend(TokenStream::from(TokenTree::Ident(idx_var_name.clone())));
+            output.extend(TokenStream::from(TokenTree::Punct(Punct::new('+', Spacing::Joint))));
+            output.extend(TokenStream::from(TokenTree::Punct(Punct::new('=', Spacing::Alone))));
+            output.extend(TokenStream::from(TokenTree::Literal(loop_step.clone())));
+            output.extend(TokenStream::from(TokenTree::Punct(Punct::new(';', Spacing::Alone))));
+        }
+    }
+
+    output.extend(TokenStream::from(TokenTree::Group(block.clone())));
+
+    output = TokenStream::from(TokenTree::Group(Group::new(Delimiter::Brace, output)));
 
     output
 }
