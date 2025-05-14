323f838801947a2bff404d14b4b73deed9937eb9
SXX <sxx1136965276@gmail.com>
2025-05-15 00:26:30 +0800

    update poly1305 implementation

--- src/crypto/poly1305/mod.rs
+++ src/crypto/poly1305/mod.rs
@@ -1,3 +1,4 @@
+use crate::utils::IntTraits;
 
 #[derive(Clone)]
 pub struct Poly1305 {
@@ -48,7 +49,7 @@ impl Poly1305 {
         let h2 = self.h[2];
 
         let (mut h0, carry) = h0.overflowing_add(t0);
-        let (mut h1, carry) = adcs(h1, t1, carry as u64);
+        let (mut h1, carry) = adcs(h1, t1, carry);
         let mut h2 = adc(h2, hibit, carry); // the 128th bit
 
         /* h *= r */
@@ -58,18 +59,18 @@ impl Poly1305 {
         d3 = umulh64(h1, self.r[0]);
         let (d1, carry) = d1.overflowing_add(d2);
         d2 = h2 * self.r[0];
-        d2 = adc(d2, d3, carry as u64);
+        d2 = adc(d2, d3, carry);
 
         d3 = h0.wrapping_mul(self.r[1]);
         h0 = umulh64(h0, self.r[1]);
         let (d1, carry) = d1.overflowing_add(d3);
         d3 = h1.wrapping_mul(self.r[1]);
         h1 = umulh64(h1, self.r[1]);
-        let (mut d3, carry) = adcs(d3, h0, carry as u64);
         h2 = h2.wrapping_mul(self.r[1]);
-        h2 = adc(h2, h1, carry as u64);
+        let (mut d3, carry) = adcs(d3, h0, carry);
+        h2 = adc(h2, h1, carry);
         let (mut d2, carry) = d2.overflowing_add(d3);
-        d3 = adc(h2, 0, carry as u64);
+        d3 = adc(h2, 0, carry);
 
         /* (partial) h %= p */
         h2 = d2 & 3;
@@ -77,10 +78,10 @@ impl Poly1305 {
         d2 = (d2 >> 2) | (d3 << 62);
         let (h0, carry) = h0.overflowing_add(d0);
         d0 = d3 >> 2;
-        h1 = adc(d3, d0, carry as u64);
+        h1 = adc(d3, d0, carry);
         let (h0, carry) = h0.overflowing_add(d2);
-        let (h1, carry) = adcs(h1, d1, carry as u64);
-        h2 = adc(h2, 0, carry as u64);
+        let (h1, carry) = adcs(h1, d1, carry);
+        h2 = adc(h2, 0, carry);
         
         self.h[0] = h0;
         self.h[1] = h1;
@@ -93,21 +94,11 @@ impl Poly1305 {
         let mut mlen = m.len();
 
         while mlen >= Self::BLOCK_LEN * 4 {
-            let chunk = unsafe { crate::utils::slice_to_array_at(m, start) };
-            self.block(&chunk, Self::HIT_BITS);
-            start += Self::BLOCK_LEN;
-
-            let chunk = unsafe { crate::utils::slice_to_array_at(m, start) };
-            self.block(&chunk, Self::HIT_BITS);
-            start += Self::BLOCK_LEN;
-
-            let chunk = unsafe { crate::utils::slice_to_array_at(m, start) };
-            self.block(&chunk, Self::HIT_BITS);
-            start += Self::BLOCK_LEN;
-
-            let chunk = unsafe { crate::utils::slice_to_array_at(m, start) };
-            self.block(&chunk, Self::HIT_BITS);
-            start += Self::BLOCK_LEN;
+            crate::const_loop!(_, 0, 4, {
+                let chunk = unsafe { crate::utils::slice_to_array_at(m, start) };
+                self.block(&chunk, Self::HIT_BITS);
+                start += Self::BLOCK_LEN;
+            });
 
             mlen -= Self::BLOCK_LEN * 4;
         }
@@ -130,53 +121,11 @@ impl Poly1305 {
 
     #[inline(always)]
     pub fn update_16_blocks(&mut self, m: &[u8; 256]) {
-        let chunk = unsafe { crate::utils::slice_to_array_at(m, 0) };
-        self.block(&chunk, Self::HIT_BITS);
-
-        let chunk = unsafe { crate::utils::slice_to_array_at(m, 16) };
-        self.block(&chunk, Self::HIT_BITS);
-
-        let chunk = unsafe { crate::utils::slice_to_array_at(m, 32) };
-        self.block(&chunk, Self::HIT_BITS);
-
-        let chunk = unsafe { crate::utils::slice_to_array_at(m, 48) };
-        self.block(&chunk, Self::HIT_BITS);
-
-        let chunk = unsafe { crate::utils::slice_to_array_at(m, 64) };
-        self.block(&chunk, Self::HIT_BITS);
-
-        let chunk = unsafe { crate::utils::slice_to_array_at(m, 80) };
-        self.block(&chunk, Self::HIT_BITS);
-
-        let chunk = unsafe { crate::utils::slice_to_array_at(m, 96) };
-        self.block(&chunk, Self::HIT_BITS);
-
-        let chunk = unsafe { crate::utils::slice_to_array_at(m, 112) };
-        self.block(&chunk, Self::HIT_BITS);
-
-        let chunk = unsafe { crate::utils::slice_to_array_at(m, 128) };
-        self.block(&chunk, Self::HIT_BITS);
-
-        let chunk = unsafe { crate::utils::slice_to_array_at(m, 144) };
-        self.block(&chunk, Self::HIT_BITS);
-
-        let chunk = unsafe { crate::utils::slice_to_array_at(m, 160) };
-        self.block(&chunk, Self::HIT_BITS);
-
-        let chunk = unsafe { crate::utils::slice_to_array_at(m, 176) };
-        self.block(&chunk, Self::HIT_BITS);
-
-        let chunk = unsafe { crate::utils::slice_to_array_at(m, 192) };
-        self.block(&chunk, Self::HIT_BITS);
-
-        let chunk = unsafe { crate::utils::slice_to_array_at(m, 208) };
-        self.block(&chunk, Self::HIT_BITS);
-
-        let chunk = unsafe { crate::utils::slice_to_array_at(m, 224) };
-        self.block(&chunk, Self::HIT_BITS);
-        
-        let chunk = unsafe { crate::utils::slice_to_array_at(m, 240) };
-        self.block(&chunk, Self::HIT_BITS);
+        #[crate::loop_unroll(i, 0, 16, 16)]
+        fn loop_unroll() {
+            let chunk = unsafe { crate::utils::slice_to_array_at(m, i) };
+            self.block(&chunk, Self::HIT_BITS);
+        }
     }
 
     #[inline(always)]
@@ -192,9 +141,9 @@ impl Poly1305 {
         let g2 = g2.wrapping_sub(1 << 2);
 
         /* select h if h < p, or h + -p if h >= p */
-        let sign_bit = (g2 >> (64 - 1)) as u32;
-        h0 = if sign_bit == 0 { g0 } else { h0 };
-        h1 = if sign_bit == 0 { g1 } else { h1 };
+        let sign_bit = !g2.sign_bit();
+        h0 = if sign_bit { g0 } else { h0 };
+        h1 = if sign_bit { g1 } else { h1 };
         // h2 = if sign_bit == 0 { g2 } else { h2 };
 
         /* h = (h + pad) */
@@ -203,7 +152,7 @@ impl Poly1305 {
 
         let (h0, carry) = h0.overflowing_add(t0);
         /* mac = h % (2^128) */
-        let h1 = adc(h1, t1, carry as u64);
+        let h1 = adc(h1, t1, carry);
 
         let mut tag = [0u8; Self::TAG_LEN];
         tag[0..8].copy_from_slice(&h0.to_le_bytes());
@@ -218,15 +167,17 @@ fn umulh64(a: u64, b: u64) -> u64 {
 }
 // Use carrying_add if bigint_helper_methods feature is available
 #[inline(always)]
-fn adcs(a: u64, b: u64, c: u64) -> (u64, u64) {
-    let (res, carry0) = a.overflowing_add(b);
-    let (res, carry1) = res.overflowing_add(c);
-    (res, (carry0 | carry1) as u64)
+fn adcs(a: u64, b: u64, c: bool) -> (u64, bool) {
+    let res = a;
+    let (res, carry0) = res.overflowing_add(b);
+    let (res, carry1) = res.overflowing_add(c as u64);
+    // SAFETY: Only one of carry0 and carry1 can be true
+    (res, unsafe { crate::utils::disjoint_or(carry0, carry1) })
 }
 
 #[inline(always)]
-fn adc(a: u64, b: u64, c: u64) -> u64 {
-    a.wrapping_add(b).wrapping_add(c)
+fn adc(a: u64, b: u64, c: bool) -> u64 {
+    a.wrapping_add(b).wrapping_add(c as u64)
 }
 
 /*
