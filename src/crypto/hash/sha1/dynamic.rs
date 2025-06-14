31b21fa001d88a37160d15cc1afde6339618055d
SXX <sxx1136965276@gmail.com>
2025-06-14 20:57:03 +0800

    rename sha1 arm to arm_ni

--- src/crypto/hash/sha1/dynamic.rs
+++ src/crypto/hash/sha1/dynamic.rs
@@ -2,7 +2,7 @@ use super::soft;
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 use super::x86;
 #[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
-use super::arm;
+use super::arm_ni;
 
 #[derive(Clone, Copy)]
 pub union Sha1 {
@@ -10,7 +10,7 @@ pub union Sha1 {
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     x86_sha: x86::Sha1,
     #[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
-    arm_sha: arm::Sha1,
+    arm_sha: arm_ni::Sha1,
 }
 
 static mut IDX: u32 = u32::MAX; // 0: soft, 1: x86/arm
@@ -19,8 +19,8 @@ static mut IDX: u32 = u32::MAX; // 0: soft, 1: x86/arm
 unsafe fn init_idx() {
     if IDX == u32::MAX {
         if crate::is_hw_feature_detected!(
-            "x86" => ("sse2", "sha"),
-            "x86_64" => ("sse2", "sha"),
+            "x86" => ("sse2", "ssse3", "sha"),
+            "x86_64" => ("sse2", "ssse3", "sha"),
             "aarch64" => ("sha2"),
             "arm" => ("neon", "sha2")
         ) {
@@ -49,7 +49,7 @@ impl Sha1 {
                 },
                 #[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
                 1 => Sha1 {
-                    arm_sha: arm::Sha1::new(),
+                    arm_sha: arm_ni::Sha1::new(),
                 },
                 _ => unreachable!(),
             }
@@ -92,7 +92,7 @@ impl Sha1 {
                 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
                 1 => x86::Sha1::oneshot(data),
                 #[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
-                1 => arm::Sha1::oneshot(data),
+                1 => arm_ni::Sha1::oneshot(data),
                 _ => unreachable!(),
             }
         }
