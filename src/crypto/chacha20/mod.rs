e1285f7c0155034a87496cfa9a3ed704e5f1d41e
SXX <sxx1136965276@gmail.com>
2025-06-17 16:20:11 +0800

    add mod.rs

---
+++ src/crypto/chacha20/mod.rs
@@ -0,0 +1,23 @@
+pub mod soft;
+
+pub(crate) const PARALLEL_BLOCKS: usize = 4;
+
+pub(crate) use soft::Chacha20Soft;
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+pub(crate) use soft::Chacha20SSE;
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+pub(crate) use soft::Chacha20AVX;
+#[cfg(any(target_arch = "aarch64", target_arch = "arm"))]
+pub(crate) use soft::Chacha20Neon;
+
+cfg_if::cfg_if!{
+    if #[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "avx"))] {
+        pub type Chacha20 = Chacha20AVX;
+    } else if #[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "sse2"))] {
+        pub type Chacha20 = Chacha20SSE;
+    } else if #[cfg(all(any(target_arch = "aarch64", target_arch = "arm"), target_feature = "neon"))] {
+        pub type Chacha20 = Chacha20Neon;
+    } else {
+        pub type Chacha20 = Chacha20Soft;
+    }
+}
