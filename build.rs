b891640a0c5c468a26941393d6b9b74a75da93ea
SXX <sxx1136965276@gmail.com>
2025-06-20 02:27:37 +0800

    add avx512 feature check

--- build.rs
+++ build.rs
@@ -2,10 +2,18 @@ extern crate rustc_version;
 use rustc_version::{version_meta, Channel};
 
 fn main() {
-    match version_meta().unwrap().channel {
+    let mut is_unstable = false;
+    let version_meta = version_meta().unwrap();
+    match version_meta.channel {
         Channel::Stable => (),
         Channel::Nightly | Channel::Dev | Channel::Beta => {
             println!("cargo:rustc-cfg=feature=\"nightly\"");
+            println!("cargo:rustc-cfg=nightly_feature");
+            is_unstable = true;
         }
+    }
+    // avx512 is only available in Rust 1.89 and later
+    if is_unstable || (version_meta.semver.major >= 1 && version_meta.semver.minor >= 89) {
+        println!("cargo:rustc-cfg=avx512_feature");
     }
 }
